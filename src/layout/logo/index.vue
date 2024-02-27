fbd995d80675088ecdbfc39bc0f47a8a4de39597
liuweizhong <978959516@qq.com>
2024-02-28 00:26:45 +0800

    chore: 菜单静态完成

---
+++ src/layout/logo/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div v-if='setting.logoHidden' class='logo'>
+    <img :src='setting.logo' alt=''>
+    <!--  矩阵软件  -->
+    <p>{{setting.title}}</p>
+  </div>
+</template>
+<script setup lang='ts'>
+// 引入设置标题
+import setting from '@/setting.ts'
+</script>
+<style scoped lang='scss'>
+.logo {
+  width: 100%;
+  height: $base-menu-logo-height;
+  display: flex;
+  align-items: center;
+  padding: 20px;
+  img {
+
+    width: 40px;
+    height: 40px;
+  }
+  p {
+    margin-left: 10px;
+    color: white;
+    font-size: $base-logo-title-fontSize;
+    font-family: SimHei;
+    width: 180px;
+    // 强制不换行
+    white-space:nowrap;
+    // 防止溢出
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+}
+</style>
---
+++ src/setting.ts
@@ -0,0 +1,6 @@
+// 用于项目logo|标题配置
+export default {
+  title: 'Matrix Software', // 项目标题
+  logo: '/public/logo.png', // 项目logo设置
+  logoHidden: true, // logo组件是否隐藏的设置 true 显示logo false 隐藏logo
+}
