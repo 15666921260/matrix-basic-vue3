e42ad444059c2f78668372ffe1ef5d1263b83bf6
liuweizhong <978959516@qq.com>
2024-03-03 16:45:19 +0800

    chore: 全屏模式

--- src/layout/tabbar/setting/index.vue
+++ src/layout/tabbar/setting/index.vue
@@ -6,7 +6,14 @@
     @click="refreshEvent"
     circle
   ></el-button>
-  <el-button size="small" icon="FullScreen" circle></el-button>
+  <el-tooltip
+    class="box-item"
+    effect="dark"
+    content="进入或退出全屏"
+    placement="bottom"
+  >
+    <el-button size="small" icon="FullScreen" @click='fullScreen' circle></el-button>
+  </el-tooltip>
   <el-button size="small" icon="Setting" circle></el-button>
   <img
     src="../../../../public/logo.png"
@@ -35,6 +42,19 @@ let layoutSettingStore = useLayoutSettingStore()
 // 刷新事件发生
 const refreshEvent = () => {
   layoutSettingStore.refresh = !layoutSettingStore.refresh
+}
+// 全屏按钮
+const fullScreen = () => {
+  // 对象的一个属性，可以用来判断当前是不是全屏模式 (全屏：true 不是全屏：false(其实显示的是null))
+  let full = document.fullscreenElement;
+  // 切换为全屏模式
+  if (!full){
+    // 利用根节点的方法requestFullscreen实现全屏模式
+    document.documentElement.requestFullscreen();
+  }else {
+    // 退出全屏模式
+    document.exitFullscreen();
+  }
 }
 </script>
 <script lang="ts">
