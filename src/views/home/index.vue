3b1aab9662545a580b26c38d7a7f47d8fd9e0fb1
liuweizhong <978959516@qq.com>
2024-03-06 23:06:00 +0800

    chore: 完成注销登录

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <h1>我是主页</h1>
-    <el-button type='primary' @click="testButton">接口测试按钮后端</el-button>
-    <el-button type='primary' @click="routerPath">接口测试按钮跳转</el-button>
+    <el-button type="primary" @click="testButton">接口测试按钮后端</el-button>
+    <el-button type="primary" @click="routerPath">接口测试按钮跳转</el-button>
   </div>
 </template>
 
@@ -13,7 +13,7 @@ import { useRouter } from 'vue-router'
 // 获取路由器
 let $router = useRouter()
 const routerPath = () => {
-  $router.push({ path: '/acl/role'})
+  $router.push({ path: '/acl/role' })
 }
 const testButton = () => {
   isLogin()
